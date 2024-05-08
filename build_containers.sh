55ff01a7fa36220776b993881eb6edd74cee3540
andrew <a.h.nguyen475@gmail.com>
2024-05-07 23:02:57 -0400

    EVEN BETTER ls aliases and added another comment to build cont. script

--- build_containers.sh
+++ build_containers.sh
@@ -3,6 +3,8 @@
 # Too many skill issues, just hard code a docker build command for now
 docker build -t my-images/arch -f $HOME/.dotfiles/.dockerfiles/Dockerfile.arch $HOME
 
+# idk how to make this script automatically run docker image prune
+
 ### TODO - Make this recursive!
   #		   ... although I'm not sure how worth it that would be as I have no idea
   #			   how many more personal Dockerfiles I'll be making in the future
