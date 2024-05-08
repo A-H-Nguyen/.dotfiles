a7f6cfc68a2cddea429bf99d8a321939f1e7c821
andrew <a.h.nguyen475@gmail.com>
2024-05-08 14:08:53 -0400

    Update script

--- build_containers.sh
+++ build_containers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Too many skill issues, just hard code a docker build command for now
+# TODO - Make this recursive!
+#		 ... although I'm not sure how worth it that would be as I have no idea
+#			 how many more personal Dockerfiles I'll be making in the future
 docker build -t my-images/arch -f $HOME/.dotfiles/.dockerfiles/Dockerfile.arch $HOME
 
-# idk how to make this script automatically run docker image prune
+docker image prune -f
 
-### TODO - Make this recursive!
-  #		   ... although I'm not sure how worth it that would be as I have no idea
-  #			   how many more personal Dockerfiles I'll be making in the future
