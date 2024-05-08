39555d1d63e2a2577165dd09e15e499f87731048
andrew <a.h.nguyen475@gmail.com>
2024-05-07 20:53:47 -0400

    Created initial setup.sh :)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "Hey"
+
+# Store .dotfiles path as a variable 
+df=$HOME/.dotfiles
+
+# There's probably a much better way to do this shit
+# but I can't be bothered
+ln -s "$df/.aliases"     ".aliases"
+ln -s "$df/.bashrc"      ".bashrc"
+ln -s "$df/.tmux"        ".tmux"
+ln -s "$df/.tmux.conf"   ".tmux.conf"
+ln -s "$df/.config"      ".config"
+ln -s "$df/.dockerfiles" ".dockerfiles"
+ln -s "$df/.zshrc"       ".zshrc"
+
+
