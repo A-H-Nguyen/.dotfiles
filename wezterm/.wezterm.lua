4dfe2240e77192dc753e6946699710a770bd2d94
Andrew Nguyen <andrewnguyen@utexas.edu>
2024-10-04 13:23:39 -0500

    Added custom pane commands - TODO: seperate this into a folder in .config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,70 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local act = wezterm.action
 local config = wezterm.config_builder()
+
+config.keys = {
+  {
+    key = '%',
+    mods = 'CTRL|ALT|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = '"',
+    mods = 'CTRL|ALT|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = '|',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '_',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'H',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'J',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'K',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'L',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+}
 
 config.font = wezterm.font 'MesloLGS NF'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
@@ -23,7 +85,17 @@ config.colors = {
 }
 
 config.hide_tab_bar_if_only_one_tab = true
+config.pane_focus_follows_mouse = true
 
+config.window_background_image = '/home/andrew/Pictures/volcano_manor.jpg'
+-- config.window_background_image = '/home/andrew/Pictures/spooderman.gif'
+config.window_background_image_hsb = {
+  brightness = 0.1,
+  hue = 0.5,
+  saturation = 1.05,
+}
+
+-- fancy gradient, incompatible with background image
 -- config.window_background_gradient = {
 --   orientation = 'Vertical',
 --   interpolation = 'Linear',
@@ -35,13 +107,6 @@ config.hide_tab_bar_if_only_one_tab = true
 --   },
 --   blend = 'Rgb',
 -- }
-
-config.window_background_image = '/home/andrew/Pictures/volcano_manor.jpg'
-config.window_background_image_hsb = {
-  brightness = 0.25,
-  hue = 0.5,
-  saturation = 1.05,
-}
 
 -- I think transparent terminal only looks good with tiling window managers
 -- config.window_background_opacity = 0.85
