ed39235d7ab647e9f1e6d9b4f8fffc33850abb05
Andrew Nguyen <andrewnguyen@utexas.edu>
2026-09-15 10:23:02 -0500

    WIP: matugen update. I'm gonna get rid of the submods for hyprland and waybar. It's a pain in the butt maintaining them separately.

---
+++ matugen/.config/matugen/hypr.lua
@@ -0,0 +1,10 @@
+-- Hypland LUA config colors
+-- Generated with Matugen
+
+local my_colors = {
+<* for name, value in colors *>
+  {{name}} = "{{value.default.rgba}}";
+<* endfor *>
+}
+
+return my_colors
